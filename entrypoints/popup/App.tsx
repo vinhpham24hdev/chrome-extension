b6ed78d2633e1f8c2a5844ae1176b944d76adacf
Le <letruong.troy@gmail.com>
2025-06-10 11:45:09 +0700

    Add login component

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,34 +1,13 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { useState } from "react";
+
+import LoginComponent from "../../components/LoginComponent";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
+    <div className="min-w-[320px] max-w-[380px] bg-gray-100 p-4">
+      <LoginComponent />
+    </div>
   );
 }
 
